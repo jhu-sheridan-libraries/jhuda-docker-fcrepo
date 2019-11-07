06a3e5ef192f0d5323892cc1af0ff5284682d41f
Elliot Metsger <emetsger@jhu.edu>
2019-11-07 13:00:16 -0500

    Don't remember why this initialization block is here, or why it is needed.

--- init-fedora.sh
+++ init-fedora.sh
@@ -12,28 +12,6 @@ then
   mkdir ${FCREPO_DATA_DIR}
 fi
 
-is_empty ${FCREPO_DATA_DIR}
-
-if [ $? -eq 0 ] ;
-then
-  echo "Initializing Fedora..."
-  apk -q --no-progress add curl
-  java -jar start.jar -Djetty.http.port=${FCREPO_JETTY_PORT} 2>/dev/null 1>/dev/null &
-
-  fedora_up 30
-
-  if [ $? -ne 0 ] ;
-  then
-    echo "Exiting."
-    exit 1;
-  fi
-
-  sleep 2
-  apk -q del curl
-  killall java
-  echo "Initialization complete..."
-fi
-
 echo "Starting Fedora..."
 java -jar start.jar \
   -Djetty.http.port=${FCREPO_JETTY_PORT} \
